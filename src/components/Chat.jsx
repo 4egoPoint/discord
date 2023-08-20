5518828078f45e89464d133a1146ab4a78a23b34
ego <mainfhr@gmail.com>
2023-08-20 23:11:52 +0200

    created prj

---
+++ src/components/Chat.jsx
@@ -0,0 +1,8 @@
+import '../index.css'
+
+
+export default function Chat() {
+   return (
+      <div className='chat'>chat</div>
+   )
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+import '../index.css'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+export default function SideBar() {
+   return (
+      <div className='sideBar'>
+         <div className="sideBar__top">
+            <h3>Employment project</h3>
+            <ExpandMoreIcon className='asd' />
+         </div>
+         
+      </div>
+   )
+}
